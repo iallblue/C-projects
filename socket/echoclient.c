fd74d1106003d93ff21c17ef59e4d53a909952fa
allblue <2572727989@qq.com>
2017-04-11 23:54:37 +0800

    多进程 - sleep

--- socket/echoclient.c
+++ socket/echoclient.c
@@ -1,64 +1,48 @@
-#include "csapp.h"
+#include "sockethelp.c"
 
-int main(int argc,char **argv) {
-	int clientfd,port;
-	char *host,buf[MAXLINE];
-	rio_t rio;
+/******
+ *
+ *	客户端设置好 要连接的主机地址 和 服务端口
+ *
+ *****/
 
-	if (argc != 3) {
-		printf("error !!\n");
+int main(int argc, char **argv)
+{
+	//定义变量
+	int clientfd;
+	char *hostname, *port;
+	char send_buf[MAXLINE],receive_buf[MAXLINE];
+	my_rio_t rio;
+	
+	if (argc != 3)
+	{
+		fprintf(stderr, "%s : arguments error\n",argv[0]);
 		exit(0);
 	}
-	
-	host = argv[1];
-	port = atoi(argv[2]);
-	
-	clientfd = open_clientfd(host,port);
-	Rio_readinitb(&rio,clientfd);
-	
-	while (fgets(buf,MAXLINE,stdin) != NULL) {
-		Rio_writen(clientfd,buf,strlen(buf));
-		Rio_readlineb(&rio,buf,MAXLINE);
-		fputs(buf,stdout);
+
+	hostname = argv[1];
+	port = argv[2];
+	//利用预先定义的方法，创建clientfd(函数正常返回时，说明连接已经建立成功)
+	//---------------------应该用带有错误处理的函数，需要完善----------
+	clientfd = Open_clientfd(hostname, port);
+	//	printf("clientfd is %d", clientfd);
+	//	exit(0);
+	//将clietnd 和rio绑定 初始化
+	my_rio_readinitb(&rio, clientfd);
+
+	//读取输出的信息
+	while (fgets(send_buf, MAXLINE, stdin) != NULL)
+	{
+		//信息写入到clientfd，发送给服务端的过程
+		my_writen(clientfd,send_buf, strlen(send_buf));
+		
+		//读取服务端发来的消息(读取到receive_buf中)
+		my_rio_readlineb(&rio, receive_buf, MAXLINE);
+		
+		//将收到的信息输出出来
+		fputs(receive_buf, stdout);
 	}
 	
 	close(clientfd);
-	exit(0);
-	
+	return 0;
 }
-
-/*
-#include "csapp.h"
-
-
-int main(int argc, char **argv) 
-{
-	int clientfd, port;
-	char *host, buf[MAXLINE];
-	rio_t rio;
-	
-	if (argc != 3)
-		{
-			fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
-			exit(0);
-		}
-		
-		host = argv[1];
-		port = atoi(argv[2]);
-		printf("----000\n");
-		clientfd = Open_clientfd(host, port);
-		printf("----1111\n");
-		Rio_readinitb(&rio, clientfd);
-		
-		while (Fgets(buf, MAXLINE, stdin) != NULL)
-		{
-			Rio_writen(clientfd, buf, strlen(buf));
-		printf("---test---\n");
-			Rio_readlineb(&rio, buf, MAXLINE);
-			Fputs(buf, stdout);
-		}
-	  Close(clientfd);
-	  exit(0);
-}
-
-*/
--- socket/echoserver.c
+++ socket/echoserver.c
@@ -1,44 +1,147 @@
-#include "csapp.h"
+#include "sockethelp.c"
+#include <signal.h>
 
-void echo (int connfd);
-int main(int argc,char **argv) {
-	
-	int listenfd,connfd;
-	socklen_t clientlen;
-	struct sockaddr_storage clientaddr;
-	char client_hostname[MAXLINE],client_port[MAXLINE];
-	
-	if (argc != 2) {
-		printf("error!!!\n ");
-		exit(0);
-	}
-	
-	listenfd = Open_listenfd(atoi(argv[1]));
-	while (1) {
-		clientlen = sizeof(struct sockaddr_storage);
-		connfd = Accept(listenfd,(SA *)&clientaddr,&clientlen);
-		getnameinfo((SA *)&clientaddr,clientlen,client_hostname,MAXLINE,client_port,MAXLINE,0);
-		printf("Connect to11 (%s %s)\n",client_hostname,client_port);
-		echo(connfd);
-		Close(connfd);
-	}
-	exit(0);
-}
+/********************************************************************
+ *
+ *
+ *	socket相关的辅助函数，可以直接使得客户端连接上，或者服务端开启监听
+ * 
+ *
+ ********************************************************************/
 
-void echo (int connfd) {
+
+
+//注意当双发建立连接后，客户端是通过clientfd进行和服务端通信的，服务端是通过connfd和客户端通信的
+void dothing(int connfd)
+{
 	size_t n;
 	char buf[MAXLINE];
-	char myword[100] = "i come from the big earth\n";
-	char tmp[100] = "-from server response:";
-	rio_t rio;
+	char send_buf[MAXLINE];
+	my_rio_t rio; //类似于文件描述符
 
-	Rio_readinitb(&rio,connfd);
-	while ((n = Rio_readlineb(&rio,buf,MAXLINE)) != 0) {
-		printf("server received %d bytes\n",(int)n);
-		//合并两个数组
-		//buf[1] = 'A';
-		//strcpy(buf,tmp);
-		//Rio_writen(connfd,buf,n);
-		Rio_writen(connfd,myword,100);
+	//(已连接)文件描述符和rio 绑定，便于读取信息
+	my_rio_readinitb(&rio, connfd);
+
+
+	//此时的设计为当客户端发送消息后就给客户端回复一条消息
+	//读取客户端发来的信息(此时已经将connfd的读取转换为 对rio的读取，这样读更加强壮)
+	
+	
+	while ((n = my_rio_readlineb(&rio,buf, MAXLINE )) != 0)
+	{
+		printf("server received %d bytes \n", n);
+		//清空缓冲区
+		sprintf(send_buf,"you send: %s", buf);
+		//给客户端发送信息
+		my_writen(connfd, send_buf,strlen(send_buf));
+		//my_writen(connfd, buf, count);
+		
 	}
+	
+	//int i = 0;
+
+	/**
+	while (1)
+	{
+		//当客户端连接上服务端时，就输出信息
+		//printf("i am sleep\n");
+		//字符数组的赋值操作
+		sprintf(send_buf, "\nbye ~~~~");
+		my_writen(connfd, send_buf, strlen(send_buf));
+		//	sleep(1);
+		//	i++;
+		//	exit(0);
+		//sleep(3);
+	}	
+	*/
+}
+
+/*
+//包含有错误处理的accept()
+int Accept(int listenfd, struct sockaddr *addr, socklen_t *addrlen)
+{
+	int rc;
+	char *err = "unix_error";
+	if ((rc = accept(listenfd, addr, addrlen)) < 0)
+	{
+		fprintf(stderr, "%s : %s\n", err, strerror(errno));
+		exit(0);
+	}
+	return rc;
+}
+*/
+
+//信号处理程序
+void sigchld_handler(int sig)
+{
+
+	//回收多个僵死的子进程
+	while (waitpid(-1, 0, WNOHANG) > 0)
+		;
+	return;
+}
+
+int main()
+{	
+	int clientfd,listenfd,connfd; //connfd 是连接描述符
+	char *hostname, *port, client_port[MAXLINE], client_hostname[MAXLINE];	
+
+	//服务端需要的参数
+	socklen_t clientlen; //客户端套接字长度
+	struct sockaddr_storage clientaddr;//没有声明为sockaddr_in 因为sockaddr_storage是一个更加通用的套接字结构
+		
+	
+	//hostname = "www.baidu.com";
+	//更好的处理是将port转换为数字，字符串容易出现bug
+	port = "8088";
+	//客户端使用的clientfd
+	//clientfd = open_clientfd(hostname, port);
+
+	//----------------- 测试服务器 ---------------
+	//监听描述符
+	listenfd = Open_listenfd(port);
+
+	//----------------------------------------------
+	//	更改为多进程
+	//----------------------------------------------
+	
+	//信号处理程序
+	signal(SIGCHLD, sigchld_handler);	
+
+	while (1)
+	{
+		clientlen = sizeof(struct sockaddr_storage); 
+		//------------------------------ 注意struct sockaddr * ---------------------------
+		//connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+		
+		//显示监听提示信息
+		printf("Listen port: %s \n",port);
+
+		//包含有错误处理的函数(可用80端口测试)
+		connfd = Accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+		//printf("%d", connfd);
+		//exit(1);
+
+		if (fork() == 0)
+		{
+			//子进程关闭监听描述符
+			close(listenfd);
+			//获得客户端连接的套接字信息
+			//int res = getnameinfo((struct sockaddr *) &clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+			//服务端显示连接信息
+			//printf("getnameinfo_id :%d client: %s, port: %s connected \n",res, client_hostname, client_port);			
+		
+			
+			//服务端处理逻辑
+			dothing(connfd);
+			close(connfd);
+			exit(0);
+		}
+		
+			close(connfd);//避免内存泄露
+		
+	}
+	//printf("clientfd :%d\n", listenfd);
+	return 0;
+
 }
