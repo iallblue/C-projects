93f42f49f0a1209eef967e11926c809d693eaab4
allblue <2572727989@qq.com>
2017-05-08 21:31:30 +0800

    统计小于n之间1的个数，真心佩服PHP串的处理能力！！！

---
+++ algorithm/count_1_num/count_1_num.php
@@ -0,0 +1,14 @@
+﻿<?php
+	/**
+	 *	统计1的个数
+	 */
+	function fun($n)
+	{
+		$str = "";
+		for ($i = 1; $i <= $n; $i++)
+		{
+			$str .= $i;
+		}
+		$str2 = str_replace("1", "", $str);
+		return strlen($str) - strlen($str2);
+	}
